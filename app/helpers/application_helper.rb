fc859925384c017fab607705475f68c857c087de
Samuellekpor <samuellekpor@gmail.com>
2023-02-01 17:45:07 +0000

    Set up linters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Create the applicationHelper
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Create the applicationRecord
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end

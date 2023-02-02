a529b2c394fdf371cdaf6d26d07fda4e6088c423
Samuellekpor <samuellekpor@gmail.com>
2023-02-02 08:24:39 +0000

    Correct linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Create the applicationHelper
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Create the applicationRecord
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class

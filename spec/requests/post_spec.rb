4d468bf248f30bbe96836d8f5f38915da6d65714
Samuellekpor <samuellekpor@gmail.com>
2023-02-09 08:43:32 +0000

    Add tests for posts #index

---
+++ spec/requests/post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :request do
+  describe 'GET #index' do
+    before(:example) { get user_posts_path(1) }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders \'index\' template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'has the corresponding placeholder' do
+      expect(response.body).to include('Here is the list of all posts of a given user')
+    end
+  end
+end

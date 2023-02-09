9404dfe6795e9bf1f981db3314d1c9f8537da012
Samuellekpor <samuellekpor@gmail.com>
2023-02-09 08:34:56 +0000

    Add tests for users #show

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe User, type: :request do
       expect(response.body).to include('Here is the list of all users')
     end
   end
+
+  describe 'GET #show' do
+    before(:example) { get user_path(1) }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders \'show\' template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'has the corresponding placeholder' do
+      expect(response.body).to include('Here is the specified user')
+    end
+  end
 end

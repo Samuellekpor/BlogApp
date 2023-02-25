b3ea20fcece795c9c5e524be0855f2d5b776647a
Samuellekpor <samuellekpor@gmail.com>
2023-02-25 12:59:38 +0000

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   has_many :comments, foreign_key: :author_id, dependent: :nullify
-  has_many :likes, foreign_key: :author_id,  dependent: :nullify
-  has_many :posts, foreign_key: :author_id,  dependent: :nullify
+  has_many :likes, foreign_key: :author_id, dependent: :nullify
+  has_many :posts, foreign_key: :author_id, dependent: :nullify
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
